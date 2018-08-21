a77c2f39ac5a9f80dfc8de827c8fc6e9eef7d3fe
Jianshao <jms_wu@hotmail.com>
2018-08-21 18:27:48 +0800

    cleanup

--- webapp-example/webapp-jaeger-example/src/main/java/com/test/servlet/TracingServletContextListener.java
+++ webapp-example/webapp-jaeger-example/src/main/java/com/test/servlet/TracingServletContextListener.java
@@ -3,7 +3,7 @@ package com.test.servlet;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import io.jaegertracing.Configuration;
+import io.jaegertracing.internal.JaegerTracer;
 import io.jaegertracing.internal.propagation.B3TextMapCodec;
 import io.jaegertracing.internal.reporters.RemoteReporter;
 import io.jaegertracing.internal.samplers.ConstSampler;
@@ -16,12 +16,12 @@ public class TracingServletContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(@SuppressWarnings("unused") ServletContextEvent servletContextEvent) {
-        final Configuration configuration = new Configuration("echoService");
         final HttpSender sender = new HttpSender.Builder("http://localhost:14268/api/traces").build();
         final RemoteReporter reporter = new RemoteReporter.Builder().withSender(sender).build();
         final ConstSampler sampler = new ConstSampler(true);
         final B3TextMapCodec b3Codec = new B3TextMapCodec.Builder().build();
-        final Tracer tracer = configuration.getTracerBuilder().registerInjector(Format.Builtin.HTTP_HEADERS, b3Codec)
+        final Tracer tracer = new JaegerTracer.Builder("echo-service")
+                .registerInjector(Format.Builtin.HTTP_HEADERS, b3Codec)
                 .registerExtractor(Format.Builtin.HTTP_HEADERS, b3Codec).withReporter(reporter).withSampler(sampler)
                 .build();
         GlobalTracer.register(tracer);
