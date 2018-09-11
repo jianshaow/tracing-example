61f3cc326c767eb81b104c7acf4a9417d33bea83
Jianshao Wu <787903+JianshaoWu@users.noreply.github.com>
2018-09-12 03:32:17 +0800

    add service

--- spring-example/spring-boot-jaeger-example/src/main/java/com/test/tracing/springboot/Application.java
+++ spring-example/spring-boot-jaeger-example/src/main/java/com/test/tracing/springboot/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JaegerExampleApplication {
+public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JaegerExampleApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
