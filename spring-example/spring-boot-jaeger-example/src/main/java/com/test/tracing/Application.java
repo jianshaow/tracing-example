73631c9839d4671d5cd09accb780675f5e4671c9
Jianshao <jms_wu@hotmail.com>
2018-09-12 13:52:23 +0800

    complete spring boot config

--- spring-example/spring-boot-jaeger-example/src/main/java/com/test/tracing/Application.java
+++ spring-example/spring-boot-jaeger-example/src/main/java/com/test/tracing/Application.java
@@ -1,4 +1,4 @@
-package com.test.tracing.springboot;
+package com.test.tracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ spring-example/spring-boot-jaeger-example/src/main/java/com/test/tracing/config/DatasourceConfig.java
@@ -0,0 +1,20 @@
+package com.test.tracing.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+public class DatasourceConfig {
+
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "c3p0")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().type(ComboPooledDataSource.class).build();
+    }
+}
