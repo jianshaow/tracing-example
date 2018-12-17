09e20e545989d3d115ed7396e8bc86d9d23848ac
Jianshao <jms_wu@hotmail.com>
2018-12-17 15:52:48 +0800

    update db config

---
+++ spring-example/spring-jaeger-example/src/main/java/com/test/tracing/config/DBConfig.java
@@ -0,0 +1,23 @@
+package com.test.tracing.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+
+@Configuration
+public class DBConfig {
+
+    @Bean
+    public DataSource dataSource(@Value("jdbc/test.ds") String jndiName) {
+        return new JndiDataSourceLookup().getDataSource(jndiName);
+    }
+
+    @Bean
+    public JdbcTemplate JdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
