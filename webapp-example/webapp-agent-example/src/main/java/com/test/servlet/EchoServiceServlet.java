aeb7031f4c8f5e7f0f2b1b16db5320f0e7624470
Jianshao <jms_wu@hotmail.com>
2020-01-07 14:13:46 +0800

    add a pure web application agent tracing example

---
+++ webapp-example/webapp-agent-example/src/main/java/com/test/servlet/EchoServiceServlet.java
@@ -0,0 +1,65 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoServiceServlet extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(EchoServiceServlet.class);
+
+    private static final long serialVersionUID = -7766401686496991505L;
+
+    private HttpClient httpClient = HttpClientBuilder.create().build();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try (final InputStream in = req.getInputStream(); final PrintWriter out = resp.getWriter()) {
+            resp.setContentType("text/plain");
+            final String msg = IOUtils.toString(in, "UTF-8");
+            logger.info("be requested to echo a message: {}", msg);
+            String result = this.callAuralService(msg);
+            result = this.callMindService(result);
+            out.println(result);
+            logger.info("echo back the result: {}", result);
+            in.close();
+            out.close();
+        }
+    }
+
+    private String callAuralService(String msg) throws ClientProtocolException, IOException {
+        return call(msg, "http://localhost:8080/aural");
+    }
+
+    private String callMindService(String msg) throws ClientProtocolException, IOException {
+        return call(msg, "http://localhost:8080/mind");
+    }
+
+    private String call(String msg, final String uri)
+            throws UnsupportedEncodingException, IOException, ClientProtocolException {
+        final HttpPost request = new HttpPost(uri);
+        request.setEntity(new StringEntity(msg));
+        final HttpResponse response = httpClient.execute(request);
+        String result = null;
+        try (InputStream in = response.getEntity().getContent()) {
+            result = IOUtils.toString(in, "UTF-8");
+            in.close();
+        }
+        return result;
+    }
+}
---
+++ webapp-example/webapp-agent-example/src/main/java/com/test/servlet/MindServiceServlet.java
@@ -0,0 +1,73 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import redis.clients.jedis.Jedis;
+
+public class MindServiceServlet extends HttpServlet {
+
+    private static Logger logger = LoggerFactory.getLogger(MindServiceServlet.class);
+
+    private static final long serialVersionUID = -7766401686496991505L;
+
+    private Jedis jedis = new Jedis();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        accessDB();
+        try (final InputStream in = req.getInputStream(); final PrintWriter out = resp.getWriter()) {
+            resp.setContentType("text/plain");
+            final String msg = IOUtils.toString(in, "UTF-8");
+            out.println(msg);
+            in.close();
+            out.close();
+        }
+    }
+
+    private void accessDB() {
+        final Integer appId = 1;
+        String appName = jedis.get(appId.toString());
+        if (appName == null) {
+            DataSource ds = null;
+            try {
+                final InitialContext ctx = new InitialContext();
+                ds = (DataSource) ctx.lookup("jdbc/test.ds");
+                try (final Connection conn = ds.getConnection();
+                        final PreparedStatement stmt = conn
+                                .prepareStatement("select name from application where id = ?")) {
+                    stmt.setInt(1, appId);
+                    final ResultSet result = stmt.executeQuery();
+                    if (result.next()) {
+                        appName = result.getString(1);
+                    }
+                    stmt.close();
+                    conn.close();
+                } catch (SQLException e) {
+                    logger.error(e.getMessage(), e);
+                }
+            } catch (NamingException e) {
+                logger.error(e.getMessage(), e);
+            }
+            if (appName != null) {
+                jedis.setex(appId.toString(), 10, appName);
+            }
+        }
+    }
+}
